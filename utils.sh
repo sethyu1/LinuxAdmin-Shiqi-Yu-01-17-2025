0c1802d9c9badc563a0e6cbb84646856e1b2b829
seth <syu7@academic.rrc.ca>
2025-02-01 17:45:24 -0600

    This was commited with git-acp

--- utils.sh
+++ utils.sh
@@ -1,6 +1,16 @@
 #! /usr/bin/env bash
 
 echo "utils just ran!"
-alias ll = 'ls -laFh'
-alias list = "la -laFh"
-alias grep = 'grep -- color=auto'
+alias ll='ls -laFh'
+alias list="la -laFh"
+alias grep='grep -- color=auto'
+
+
+function git-acp(){
+    git add .
+    git commit -m 'This was commited with git-acp'
+    echo "git push would've run now!"
+    #git push
+}
+
+git-acp
